c78acc8beb9d090ccf8f33fb5cb104cb3de140e3
AsaMitaka <cepelits@gmail.com>
2024-02-12 21:43:16 +0200

    + const

---
+++ rest-countries-api/src/const/index.ts
@@ -0,0 +1,14 @@
+export enum PAGES {
+  MAIN = '/',
+  DETAIL = '/:country',
+  ERROR = '*',
+}
+
+export const REGIONS = [
+  { name: 'All', value: '' },
+  { name: 'Africa', value: 'Africa' },
+  { name: 'Americas', value: 'Americas' },
+  { name: 'Asia', value: 'Asia' },
+  { name: 'Europe', value: 'Europe' },
+  { name: 'Oceania', value: 'Oceania' },
+];
